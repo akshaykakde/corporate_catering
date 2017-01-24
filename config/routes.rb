51c42a4ec4c7d93067944d7d967998a94290e2da
amitk <amit@joshsoftware.com>
2017-01-24 19:31:22 +0530

    login done

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ Rails.application.routes.draw do
     get 'users/edit' => 'devise/registrations#edit', :as => 'edit_user_registration'
     put 'users' => 'devise/registrations#update', :as => 'user_registration'
   end
-  get '/users/welcome', to: 'users#welcome'
+  get '/users/hr_dashboard', to: 'users#hr_dashboard'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: "home#index"
 end
