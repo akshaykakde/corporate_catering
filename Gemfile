db05b99ef255b5be04dc319d5903e2960dff3de0
tejaswini-gambhire <gambhire.tejaswini30@gmail.com>
2017-01-25 23:16:33 +0530

    bootstrap added to devise forms[2]

--- Gemfile
+++ Gemfile
@@ -5,6 +5,7 @@ git_source(:github) do |repo_name|
   "https://github.com/#{repo_name}.git"
 end
 
+gem 'toastr-rails'
 gem 'devise'
 gem 'devise_invitable'
 gem 'haml-rails'
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,13 +1,11 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
-  def after_sign_in_path(resource)
-    current_user_path
-  end
-
-  private
-  
-    def authenticate_inviter!
-      current_user
+  def after_sign_in_path_for(resource)
+    if(current_user.role == "hr")
+      users_hr_dashboard_path
+    else
+      root_path
     end
+  end
 end
