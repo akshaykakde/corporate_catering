f282db17885756173f1fdaafad740df65fa82273
Your Name <you@example.com>
2017-01-27 15:49:44 +0530

    final done

--- config/routes.rb
+++ config/routes.rb
@@ -18,6 +18,5 @@ Rails.application.routes.draw do
   	resources :menu_items
   end	
   post 'vendors/select_vendors' => 'vendors#select_vendors', :as => 'select_vendors'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  # root to: "home#index"
+
 end
